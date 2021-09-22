d5bcd0a4da3a744b8c84dfd62fd46e5d40e59a3b
Asqaraliyev Faxriyor <asqaraliyev07@gmail.com>
2021-09-22 20:35:28 +0500

    bugs-fixed

--- test_main.py
+++ test_main.py
@@ -14,7 +14,7 @@ PASSWORD = 'huff-puff'
 
 @pytest.fixture
 def client():
-    os.environ['JWT_SECRET'] = SECRET
+    os.environ['TEST_JWT_SECRET'] = SECRET
     main.APP.config['TESTING'] = True
     client = main.APP.test_client()
 
